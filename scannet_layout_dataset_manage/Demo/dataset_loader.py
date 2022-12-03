5f7cdf28a0643a2b8c6d8e250cc6514d305cf511
chLi <565353780@qq.com>
2022-12-03 20:07:02 +0800

    add basic dataset queries

--- scannet_layout_dataset_manage/Demo/dataset_loader.py
+++ scannet_layout_dataset_manage/Demo/dataset_loader.py
@@ -10,4 +10,18 @@ def demo():
 
     dataset_loader = DatasetLoader(scannet_layout_dataset_folder_path,
                                    print_progress)
+
+    scene_name_list = dataset_loader.getSceneNameList()
+    print("scene_name_list")
+    print(scene_name_list)
+
+    scene_name = scene_name_list[0]
+    view_name_list = dataset_loader.getViewNameList(scene_name)
+    print("view_name_list")
+    print(view_name_list)
+
+    if len(view_name_list) > 0:
+        view_name = view_name_list[0]
+        view = dataset_loader.getView(scene_name, view_name)
+        view.outputInfo()
     return True
--- scannet_layout_dataset_manage/Module/dataset_loader.py
+++ scannet_layout_dataset_manage/Module/dataset_loader.py
@@ -9,3 +9,27 @@ class DatasetLoader(object):
     def __init__(self, dataset_folder_path, print_progress=False):
         self.dataset = Dataset(dataset_folder_path, print_progress)
         return
+
+    def getSceneNameList(self):
+        return list(self.dataset.scene_dict.keys())
+
+    def isSceneNameValid(self, scene_name):
+        return scene_name in self.dataset.scene_dict.keys()
+
+    def isViewNameValid(self, scene_name, view_name):
+        if not self.isSceneNameValid(scene_name):
+            return False
+        return view_name in self.dataset.scene_dict[scene_name].view_dict.keys(
+        )
+
+    def getViewNameList(self, scene_name):
+        assert self.isSceneNameValid(scene_name)
+        return list(self.dataset.scene_dict[scene_name].view_dict.keys())
+
+    def getScene(self, scene_name):
+        assert self.isSceneNameValid(scene_name)
+        return self.dataset.scene_dict[scene_name]
+
+    def getView(self, scene_name, view_name):
+        assert self.isViewNameValid(scene_name, view_name)
+        return self.dataset.scene_dict[scene_name].view_dict[view_name]
