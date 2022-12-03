5f36e358e0ed4ea9503d5236a19e4836053789bc
chLi <565353780@qq.com>
2022-12-03 20:06:49 +0800

    add outputInfo func

--- scannet_layout_dataset_manage/Data/view.py
+++ scannet_layout_dataset_manage/Data/view.py
@@ -3,6 +3,7 @@
 
 import os
 
+
 class View(object):
 
     def __init__(self, scene_folder_path, view_name):
@@ -53,4 +54,25 @@ class View(object):
             "polygon_vis/" + self.view_name + ".jpg"
         self.valid_masks_json_file_path = self.scene_folder_path + \
             "valid_masks/" + self.view_name + ".json"
+        return True
+
+    def outputInfo(self, info_level=0):
+        line_start = "\t" * info_level
+        print(line_start + "[View]")
+        print(line_start + "\t view_name: " + self.view_name)
+        print(line_start + "\t scene_folder_path: " + self.scene_folder_path)
+        print(line_start + "\t color_image_file_path: " +
+              self.color_image_file_path)
+        print(line_start + "\t depth_image_file_path: " +
+              self.depth_image_file_path)
+        print(line_start + "\t labels_json_file_path: " +
+              self.labels_json_file_path)
+        print(line_start + "\t layout_depth_npy_file_path: " +
+              self.layout_depth_npy_file_path)
+        print(line_start + "\t layout_depth_vis_image_file_path: " +
+              self.layout_depth_vis_image_file_path)
+        print(line_start + "\t polygon_vis_image_file_path: " +
+              self.polygon_vis_image_file_path)
+        print(line_start + "\t valid_masks_json_file_path: " +
+              self.valid_masks_json_file_path)
         return True
