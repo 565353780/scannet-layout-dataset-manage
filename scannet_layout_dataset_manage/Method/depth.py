6c9257c5e8fd2c0bcd25f1cec2dcfaddb9873fff
chLi <565353780@qq.com>
2022-12-04 18:10:02 +0800

    add get images algo

--- scannet_layout_dataset_manage/Method/depth.py
+++ scannet_layout_dataset_manage/Method/depth.py
@@ -5,40 +5,52 @@ import cv2
 import numpy as np
 
 
-def getDepthPoints(view):
+def getDepthImage(view):
     depth_image_file_path = view.depth_image_file_path
     depth_image = cv2.imread(depth_image_file_path)
+    depth_image = depth_image[:, :, 0]
+    return depth_image
 
+
+def getLayoutDepthImage(view):
+    layout_depth_npy_file_path = view.layout_depth_npy_file_path
+    layout_depth_image = np.load(layout_depth_npy_file_path)
+    return layout_depth_image
+
+
+def getUniformDepthImages(view, depth_scale=1):
+    depth_image = getDepthImage(view).astype(np.float64)
+    depth_max = np.max(depth_image)
+    if depth_scale != 1:
+        depth_image *= depth_scale
+
+    layout_depth_image = getLayoutDepthImage(view)
+    layout_depth_max = np.max(layout_depth_image)
+    layout_depth_image *= depth_scale * depth_max / layout_depth_max
+    return depth_image, layout_depth_image
+
+
+def getPointsFromImage(image):
     points = []
-    for i in range(depth_image.shape[0]):
-        for j in range(depth_image.shape[1]):
-            depth = depth_image[i][j][0]
-            points.append([i, j, depth])
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            points.append([i, j, image[i][j]])
     points = np.array(points)
     return points
+
+
+def getDepthPoints(view):
+    depth_image = getDepthImage(view)
+    return getPointsFromImage(depth_image)
 
 
 def getLayoutDepthPoints(view):
-    layout_depth_npy_file_path = view.layout_depth_npy_file_path
-    layout_depth_image = np.load(layout_depth_npy_file_path)
-
-    points = []
-    for i in range(layout_depth_image.shape[0]):
-        for j in range(layout_depth_image.shape[1]):
-            depth = layout_depth_image[i][j]
-            points.append([i, j, depth])
-    points = np.array(points)
-
-    return points
+    layout_depth_image = getLayoutDepthImage(view)
+    return getPointsFromImage(layout_depth_image)
 
 
 def getUniformDepthPoints(view, depth_scale=1):
-    depth_points = getDepthPoints(view)
-    depth_max = np.max(depth_points[:, 2])
-    if depth_scale != 1:
-        depth_points[:, 2] *= depth_scale
-
-    layout_depth_points = getLayoutDepthPoints(view)
-    layout_depth_max = np.max(layout_depth_points[:, 2])
-    layout_depth_points[:, 2] *= depth_scale * depth_max / layout_depth_max
+    depth_image, layout_depth_image = getUniformDepthImages(view, depth_scale)
+    depth_points = getPointsFromImage(depth_image)
+    layout_depth_points = getPointsFromImage(layout_depth_image)
     return depth_points, layout_depth_points
